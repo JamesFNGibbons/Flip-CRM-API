449cba886b3240bedbf71b78391eaee405f19619
James Gibbons <jgibbons@jgibbons-imac.local>
2018-05-13 16:44:04 +0100

    Completed create client feature

--- test.js
+++ test.js
@@ -3,11 +3,16 @@ const fca = require('./');
 console.log('Starting.');
 
 let options = {
-  host : 'cs-crm-prjct-jgibbons894449.codeanyapp.com',
+  host : 'localhost',
   port : 3000,
-  public_key : 'abcde'
+  public_key : '7cf7eba8-5d46-48a9-ac2e-6ad49128cbb5'
 };
 
-fca.authenticate(options).then((data) => {
-  
+fca.init(options, (err, result) => {
+	if(err) throw err;
+	console.log(`=> Connected to FCA As (${result.application_name})`);
+});
+
+fca.create_client({name: 'James'}).then(() => {
+	console.log('Created client');
 })
